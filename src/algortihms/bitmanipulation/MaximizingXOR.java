2c45266b5d785d6d3b5239f963e2e58dcbc36793
sandeepraikar <snraikar@gmail.com>
2016-03-15 20:17:17 -0500

    Added solution for Maximizing XOR

---
+++ src/algortihms/bitmanipulation/MaximizingXOR.java
@@ -0,0 +1,27 @@
+package algortihms.bitmanipulation;
+
+import java.util.Scanner;
+
+public class MaximizingXOR {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int l = in.nextInt();
+		int r = in.nextInt();
+		in.close();
+		int max=0;
+		int min=0;
+		
+		for(int i=l;i<=r;i++){
+			for(int j=l;j<=r;j++){
+				min=i^j;
+				if(min>max){
+					max=min;
+				}
+			}
+		}
+		System.out.println(max);		
+
+	}
+
+}
