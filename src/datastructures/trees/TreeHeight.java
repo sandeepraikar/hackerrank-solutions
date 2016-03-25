4cda2d7b4f17f253cc709c92192ca6e55839c9ad
sandeepraikar <snraikar@gmail.com>
2016-03-25 12:02:46 -0500

    Added solution for Tree height

---
+++ src/datastructures/trees/TreeHeight.java
@@ -0,0 +1,24 @@
+package datastructures.trees;
+
+public class TreeHeight {
+
+	class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+
+	int height(Node root) {
+		if (root == null) {
+			return 0;
+		} else {
+			return Math.max(height(root.left) + 1, height(root.right) + 1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
